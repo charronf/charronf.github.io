2964ab53a49e42afb75ae8ce97e57f5018af233b
fcharron-cstj <2061736@cstj.qc.ca>
2024-09-23 19:51:05 -0400

    updated page content

--- vue.js
+++ vue.js
@@ -1,12 +1,23 @@
 import { createApp } from "https://unpkg.com/vue@3/dist/vue.esm-browser.js";
 createApp({
   data() {
-    return {};
+    return {
+      mouseX : '10px',
+      mouseY : '10px',
+    };
   },
+  computed : {
+    gradiant(){
+      return {backgroundImage : `radial-gradient(circle at ${this.mouseX}% ${this.mouseY}px, #393d8a, #191a30 65%)`}
+    }
+      },
   methods: {
-    log() {
-      console.log("hello");
-    },
+    test(event){
+      let width = window.innerWidth;
+      this.mouseX = (event.clientX / width) * 100;
+      this.mouseY = event.clientY;
+    }
   },
   mounted() {},
+
 }).mount("#app");
